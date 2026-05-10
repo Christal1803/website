583dbb85e4536f2d8480267916abd9a24e4bcec3
sasidharan <sasidharan.itprof@gmail.com>
2026-05-10 16:12:42 +0530

    Config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: "https",
@@ -8,7 +10,6 @@ const nextConfig = {
       },
     ],
   },
-  output: "export",
 };
 
 export default nextConfig;
